e88e5d9c31e5076fad8712211941b63bae27a46f
dafid palevi <43629045+dafidpalevi1@users.noreply.github.com>
2024-04-14 12:45:21 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ document.getElementById("login-form").addEventListener("submit", function(event)
 
   // Contoh validasi sederhana
   if (username === "admin" && password === "admin123") {
-      // Redirect ke halaman selanjutnya setelah login berhasil
-      window.location.href = "https://timonadiyoso.netlify.app/"; // Ganti dashboard.html dengan URL halaman yang sesuai
+      // Set status login ke "true" di local storage
+      localStorage.setItem("isLoggedIn", "true");
+      // Redirect ke halaman utama setelah login berhasil
+      window.location.href = "index.html"; // Ganti index.html dengan URL halaman utama yang sesuai
   } else {
       // Tampilkan pesan error jika login gagal
       document.getElementById("error-message").innerText = "Username atau password salah!";
   }
 });
+
 
 
 
