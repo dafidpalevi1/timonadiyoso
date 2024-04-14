d0d78156a0ac2a45b8517110e131a3c9b244c6d4
dafid palevi <43629045+dafidpalevi1@users.noreply.github.com>
2024-04-14 12:36:13 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent default form submission
+
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Contoh validasi sederhana
+  if (username === "admin" && password === "admin123") {
+      // Redirect ke halaman selanjutnya setelah login berhasil
+      window.location.href = "https://timonadiyoso.netlify.app/"; // Ganti dashboard.html dengan URL halaman yang sesuai
+  } else {
+      // Tampilkan pesan error jika login gagal
+      document.getElementById("error-message").innerText = "Username atau password salah!";
+  }
+});
+
+
+
+
 /* Demo purposes only */
 $(".hover").mouseleave(
     function() {
